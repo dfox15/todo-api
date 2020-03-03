97a2582ef0adbe0b13a2f1808a52929d7d7aaf51
Donald Fox <fox_donald@yahoo.com>
2020-03-02 20:17:33 -0500

    Add DELETE todos route

--- server.js
+++ server.js
@@ -52,6 +52,19 @@ app.post('/todos', (req, res) => {
     res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', (req, res) => {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if (!matchedTodo) {
+        res.status(404).json({"error": "no todo found with that id"});
+      } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+      }
+});
+
 app.listen(PORT, () => {
   console.log("Express listening on port " + PORT + "!");
 });
