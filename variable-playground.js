8eb9429cbacfda7be9bb97fdc080ba905a9b3c5c
Donald Fox <fox_donald@yahoo.com>
2020-03-05 21:52:07 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = { 
+//     name: 'Donald',
+//     age: 21
+// }
+
+// updatePerson = (obj) => {
+//     // obj = {
+//     //     name: 'Donald',
+//     //     age: 45
+//     // };
+//     obj.age = 28;
+// };
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+addGrade = (gradesArr) => {
+    gradesArr.push(55);
+    debugger;
+};
+
+addGrade(grades)
+console.log(grades);
