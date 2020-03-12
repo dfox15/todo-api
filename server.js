cb6b2a72b1577d4780d3e484ac8d550e7e86466d
Donald Fox <fox_donald@yahoo.com>
2020-03-11 20:07:03 -0400

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var _ = require('underscore');
-var db = require('./db.js');
+var bodyParser = require("body-parser");
+var _ = require("underscore");
+var db = require("./db.js");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,7 +11,7 @@ var todoNextId = 1;
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Todo API Root");
+    res.send("Todo API Root");
 });
 
 // GET /todos?completed=true
@@ -19,97 +19,100 @@ app.get("/todos", (req, res) => {
     var queryParams = req.query;
     var filteredTodos = todos;
 
-    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {completed: true});
-    } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {completed: false});
+    if (
+        queryParams.hasOwnProperty("completed") &&
+        queryParams.completed === "true"
+    ) {
+        filteredTodos = _.where(filteredTodos, { completed: true });
+    } else if (
+        queryParams.hasOwnProperty("completed") &&
+        queryParams.completed === "false"
+    ) {
+        filteredTodos = _.where(filteredTodos, { completed: false });
     }
 
-    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-      filteredTodos = _.filter(filteredTodos, (todo) => {
-        return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-      });
+    if (queryParams.hasOwnProperty("q") && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, todo => {
+            return (
+                todo.description
+                    .toLowerCase()
+                    .indexOf(queryParams.q.toLowerCase()) > -1
+            );
+        });
     }
-  // Alternative to avoid using JSON.stringify() or JSON.parse().
-  // This takes the parameter we want sent back to the caller (client/browser).
-  // The conversion to JSON is implicit  
-  res.json(filteredTodos);
+    // Alternative to avoid using JSON.stringify() or JSON.parse().
+    // This takes the parameter we want sent back to the caller (client/browser).
+    // The conversion to JSON is implicit
+    res.json(filteredTodos);
 });
 
 // GET /todos/:id
 app.get("/todos/:id", (req, res) => {
-  var todoId = parseInt(req.params.id, 10);
+    var todoId = parseInt(req.params.id, 10);
 
-  var matchedTodo = _.findWhere(todos, {id: todoId});
-
-  if (matchedTodo) {
-    res.json(matchedTodo);
-  } else {
-    res.status(404).send();
-  }
+    db.todo.findByPk(todoId).then((todo) => {
+      if (!!todo) {
+          res.json(todo.toJSON());
+        } else {
+            res.status(404).send();
+        }
+    }, (e) => {
+      res.status(500).send();
+    })
 });
 
 // POST /todos
-app.post('/todos', (req, res) => {
-    var body = _.pick(req.body, 'description', 'completed');
+app.post("/todos", (req, res) => {
+    var body = _.pick(req.body, "description", "completed");
 
-    db.todo.create(body).then((todo) => {
-      res.json(todo.toJSON());
-    }, (e) => {
-      res.status(400).json(e);
-    });
-      
-  //});
-    // if(!_.isBoolean(body.completed) 
-    //     || !_.isString(body.description) 
-    //     || body.description.trim().length === 0) {
-    //     return res.status(400).send();
-    // }
-
-    // body.description = body.description.trim();
-    // body.id =  todoNextId++;
-
-    // todos.push(body);
-
-    // res.json(body);
+    db.todo.create(body).then(
+        todo => {
+            console.log(todo);
+            res.json(todo.toJSON());
+        },
+        e => {
+            res.status(400).json(e);
+        }
+    );
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', (req, res) => {
+app.delete("/todos/:id", (req, res) => {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var matchedTodo = _.findWhere(todos, { id: todoId });
 
     if (!matchedTodo) {
-        res.status(404).json({"error": "no todo found with that id"});
-      } else {
+        res.status(404).json({ error: "no todo found with that id" });
+    } else {
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
-      }
+    }
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', (req, res) => {
-    var body = _.pick(req.body, 'description', 'completed');
+app.put("/todos/:id", (req, res) => {
+    var body = _.pick(req.body, "description", "completed");
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-    var validAttributes = {
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+    var validAttributes = {};
 
-    };
-
-    if(!matchedTodo) {
+    if (!matchedTodo) {
         return res.status(404).send();
     }
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    if (body.hasOwnProperty("completed") && _.isBoolean(body.completed)) {
         validAttributes.completed = body.completed;
-    } else if(body.hasOwnProperty('completed')) {
+    } else if (body.hasOwnProperty("completed")) {
         return res.status(400).send();
     }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) 
-        && body.description.trim().length > 0) {
-            validAttributes.description = body.description;
-    } else if(body.hasOwnProperty('description')) {
+    if (
+        body.hasOwnProperty("description") &&
+        _.isString(body.description) &&
+        body.description.trim().length > 0
+    ) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty("description")) {
         return res.status(400).send();
     }
 
@@ -118,9 +121,7 @@ app.put('/todos/:id', (req, res) => {
 });
 
 db.sequelize.sync().then(() => {
-
-  app.listen(PORT, () => {
-    console.log("Express listening on port " + PORT + "!");
-  });
-  
+    app.listen(PORT, () => {
+        console.log("Express listening on port " + PORT + "!");
+    });
 });
